0d1ba5a8340ebdbc0a75d4423f4c59c395d7039a
wintory <thanapol.sakul@gmail.com>
2025-01-10 23:28:02 +0700

    build: initial db

---
+++ index.ts
@@ -0,0 +1,20 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (_, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
